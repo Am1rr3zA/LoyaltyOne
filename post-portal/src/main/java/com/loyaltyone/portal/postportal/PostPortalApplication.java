4ec93bc3bd1ae6028180f3b4cd822c31c9c0ad3d
Amirreza Soudi <amirreza.soudi@gmail.com>
2017-12-06 00:12:21 -0500

    Create Initial Spring-Boot Backend

---
+++ post-portal/src/main/java/com/loyaltyone/portal/postportal/PostPortalApplication.java
@@ -0,0 +1,12 @@
+package com.loyaltyone.portal.postportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostPortalApplication.class, args);
+	}
+}
